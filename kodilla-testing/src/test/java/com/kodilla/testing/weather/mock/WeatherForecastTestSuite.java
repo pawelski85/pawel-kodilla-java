90523d92de4a27ac5817316a9329f8b3a13ad7ad
charlotte <nowyjuzer@gmail.com>
2020-10-27 20:56:29 +0100

    6.5 method calculateMedian added

--- kodilla-testing/src/test/java/com/kodilla/testing/weather/mock/WeatherForecastTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/weather/mock/WeatherForecastTestSuite.java
@@ -7,7 +7,10 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
 import java.util.*;
+
+import static org.mockito.ArgumentMatchers.doubleThat;
 import static org.mockito.Mockito.when;
 
 @DisplayName("TDD: Weather Forecast Test Suite")
@@ -15,8 +18,7 @@ import static org.mockito.Mockito.when;
 class WeatherForecastTestSuite {
     @Mock
     private Temperatures temperaturesMock;
-    @Mock
-    private Temperatures temperaturesMock2;
+
     private static int testCounter = 0;
 
     @BeforeAll
@@ -83,55 +85,48 @@ class WeatherForecastTestSuite {
         Assertions.assertEquals(26.56, result, 0.01);
     }
 
-    @DisplayName("when create temperaturesMap with number of sensors data, " +
-                "then calculateForecast should show the average temperatures median")
+    @DisplayName("when create OddTemperaturesMapOdd with odd number of sensors data, " +
+            "then calculateOddMedian should show the average temperatures median")
     @Test
-    void testTempMedian() {
-            //Given
-            Map<String, Double> evenTemperaturesMap = new HashMap<>();
-            evenTemperaturesMap.put("Rzeszow", 25.5);
-            evenTemperaturesMap.put("Krakow", 26.2);
-            evenTemperaturesMap.put("Wroclaw", 24.8);
-            evenTemperaturesMap.put("Warszawa", 25.2);
-            evenTemperaturesMap.put("Gdansk", 26.1);
-            evenTemperaturesMap.put("Olsztyn", 26.2);
+    void testTempMedianOdd() {
+        //Given
+        Map<String, Double> oddTemperaturesMap = new HashMap<>();
+        oddTemperaturesMap.put("Rzeszow", 25.5);
+        oddTemperaturesMap.put("Krakow", 26.2);
+        oddTemperaturesMap.put("Wroclaw", 24.8);
+        oddTemperaturesMap.put("Warszawa", 25.2);
+        oddTemperaturesMap.put("Gdansk", 26.1);
 
-            Map<String, Double> oddTemperaturesMap = new HashMap<>();
-            oddTemperaturesMap.put("Rzeszow", 25.5);
-            oddTemperaturesMap.put("Krakow", 26.2);
-            oddTemperaturesMap.put("Wroclaw", 24.8);
-            oddTemperaturesMap.put("Warszawa", 25.2);
-            oddTemperaturesMap.put("Gdansk", 26.1);
+        when(temperaturesMock.getTemperatures()).thenReturn(oddTemperaturesMap);
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
 
-            when(temperaturesMock.getTemperatures()).thenReturn(evenTemperaturesMap);
-            WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+        //When
+        double result = weatherForecast.calculateMedian();
 
-            when(temperaturesMock2.getTemperatures()).thenReturn(oddTemperaturesMap);
-            WeatherForecast weatherForecast2 = new WeatherForecast(temperaturesMock2);
+        //Then
+        Assertions.assertEquals(25.5, result, 0.01);
+    }
 
-            List<Double> tempList = new LinkedList<>();
-            for (Map.Entry<String, Double> tempMap : weatherForecast.calculateForecast().entrySet()) {
-                tempList.add(tempMap.getValue());
-            }
+    @DisplayName("when create evenTemperaturesMap with even number of sensors data, " +
+            "then calculateEvenMedian should show the average temperatures median")
+    @Test
+    void testTempMedianEven() {
+        //Given
+        Map<String, Double> evenTemperaturesMap = new HashMap<>();
+        evenTemperaturesMap.put("Rzeszow", 25.5);
+        evenTemperaturesMap.put("Krakow", 26.2);
+        evenTemperaturesMap.put("Wroclaw", 24.8);
+        evenTemperaturesMap.put("Warszawa", 25.2);
+        evenTemperaturesMap.put("Gdansk", 26.1);
+        evenTemperaturesMap.put("Olsztyn", 26.2);
 
-            List<Double> tempList2 = new LinkedList<>();
-            for (Map.Entry<String, Double> tempMap2 : weatherForecast2.calculateForecast().entrySet()) {
-                tempList2.add(tempMap2.getValue());
-            }
+        when(temperaturesMock.getTemperatures()).thenReturn(evenTemperaturesMap);
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
 
-            Collections.sort(tempList);
-            Collections.sort(tempList2);
+        //When
+        double resultEven = weatherForecast.calculateMedian();
 
-            //When
-            int tempMedian1 = ((weatherForecast.calculateForecast().size() / 2) - 1);
-            int tempMedian2 = ((weatherForecast.calculateForecast().size() / 2));
-            double resultEven = (tempList.get(tempMedian1) + tempList.get(tempMedian2)) / 2;
-
-            int tempMedianOdd = (weatherForecast2.calculateForecast().size() / 2);
-            double resultOdd = tempList2.get(tempMedianOdd);
-
-            //Then
-            Assertions.assertEquals(26.8, resultEven,0.01);
-            Assertions.assertEquals(26.5, resultOdd, 0.01);
+        //Then
+        Assertions.assertEquals(25.8, resultEven, 0.01);
     }
 }
